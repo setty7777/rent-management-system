c21bf21b51451c1f11f22e4fc3d1bdaf9de85ba1
setty7777 <icuraja130201@gmail.com>
2026-05-06 19:36:38 +0530

    Update server.js

--- rent-management-backend/server.js
+++ rent-management-backend/server.js
@@ -41,7 +41,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://127.0.0.1:3000",
-  "https://demo-production-bf0f.up.railway.app"
+  "https://rent-management-system-as6t.onrender.com"
 ];
 
 app.use(cors({
